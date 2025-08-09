528c776ef70ba33731718c53447ec8845e6aea97
Brandon Rosario-Autry <brandon.jwjp@gmail.com>
2025-08-09 08:40:00 +0000

    hmm when i click about it brings me to home, thats not right, also click

--- next.config.js
+++ next.config.js
@@ -22,15 +22,6 @@ const nextConfig = {
       }
     ],
   },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/home',
-        permanent: true,
-      },
-    ]
-  },
 };
 
 module.exports = nextConfig;
